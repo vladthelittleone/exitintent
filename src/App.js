05952f9918fe67b6a86e727f6d1ecd24766848ee
vladthelittleone <vladthelittleone@gmail.com>
2019-06-30 22:51:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
-import React, { Component } from "react";
-import { Modal, ModalBackground, ModalContent } from "./Modal";
+import React, { Component } from 'react';
+import { Modal, ModalBackground, ModalContent } from './Modal';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showExit: false };
-    // This binding is necessary to make `this` work in the callback
-    this.handleExit = this.handleExit.bind(this);
-    this.handleExitClick = this.handleExitClick.bind(this);
-  }
+const App = () => {
+  // State
+  const [setShowExit, showExit] = useState(false);
 
-  componentDidMount() {
-    document.addEventListener("mouseout", this.handleExit);
-  }
+  // Handlers
+  const handleExitClick = () => {
+    setShowExit(false);
+  };
 
-  handleExitClick(e) {
-    this.setState({
-      showExit: false,
-    });
-  }
-
-  handleExit(e) {
-    e = e ? e : window.event;
+  const handleExit = param => {
+    const e = param || window.event;
     // Get the current viewport width.
-    var vpWidth = Math.max(
+    const vpWidth = Math.max(
       document.documentElement.clientWidth,
-      window.innerWidth || 0
+      window.innerWidth || 0,
     );
 
     // If the current mouse X position is within 50px of the right edge
@@ -38,30 +28,28 @@ class App extends Component {
 
     // Reliable, works on mouse exiting window and
     // user switching active program
-    var from = e.relatedTarget || e.toElement;
+    const from = e.relatedTarget || e.toElement;
     if (!from) {
-      this.setState({
-        showExit: true,
-      });
+      setShowExit(true);
     }
-  }
+  };
 
-  render() {
-    const { showExit } = this.state;
+  useEffect(() => {
+    document.addEventListener('mouseout', handleExit);
+  });
 
-    return (
-      <Modal showExit={showExit}>
-        <ModalBackground onClick={this.handleExitClick} />
-        <ModalContent>
-          <h1>Please Don't Leave</h1>
-          <p>
-            You really should sign up to our mail list before you leave so we
-            can send you stuff and get you to spend some $ with us
-          </p>
-        </ModalContent>
-      </Modal>
-    );
-  }
-}
+  return (
+    <Modal showExit={showExit}>
+      <ModalBackground onClick={handleExitClick} />
+       <ModalContent>
+        <h1>Please Don't Leave</h1>
+        <p>
+          You really should sign up to our mail list before you leave so we
+          can send you stuff and get you to spend some $ with us
+        </p>
+       </ModalContent>
+    </Modal>
+  );
+};
 
 export default App;
